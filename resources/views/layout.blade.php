1287a1685b9b55e7401eb1854ca711bef4d21341
Jeff Nielsen <jefftoddnielsen@gmail.com>
2019-03-03 16:47:43 +1100

    Added an nicer placerholder

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,9 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Forum</title>
+    <title>Example</title>
+    <style>
+        body {
+            font-family: helvetica, sans-serif;
+            color: #333;
+            margin: 0;
+            padding: 0;
+        }
+        #app {
+            text-align: center;
+            position: absolute;
+            width: 100%;
+            top: 50%;
+            margin-top: -100px;
+        }
+    </style>
 </head>
 <body>
-	<div id="app"></div>
+	<div id="app">
+        <h1>Laravel Application Package Boilerplate</h1>
+        <p>A package boilerplate for setting up packaged applications</p>
+	</div>
 </body>
 </html>
